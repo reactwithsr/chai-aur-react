b242aa898115104a18c480ee4d6f78a23c421b48
reactwithsr <reactwithsr@gmail.com>
2025-07-04 11:06:34 +0000

    react basic complete

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,12 @@
 import Chai from "./Chai"
  function App() {
+  const name = "Shubhangi Sumant Rai"
  
   return (
+    <>
    <Chai/>
+   <h1>My Love {name}</h1>
+   </>
   )
 }
 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,47 @@
-import { StrictMode } from 'react'
+import React from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
+
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App ! Chai</h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//     type: 'a',
+//     props:{
+//         href:"https://google.com",
+//         target: '_blank'
+//     },
+//     Children: 'Click to visit Google'
+
+// }
+
+const anotherElement = (
+  <a href="https://www.google.com" target='_ blank'>visit google</a>
+)
+
+
+const dusraElement = "Tripti Pal"
+
+const ReactElement = React.createElement(
+  'a',
+  {href: 'https://www.facebook.com',target:'_blank'},
+  'click me to visit facebook',
+  dusraElement
+
+
+)
 
 createRoot(document.getElementById('root')).
 render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+
+  // anotherElement
+  ReactElement
+  // <App/>
+  
 )
